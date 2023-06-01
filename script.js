7fcf84a7a080482f0272d8a816ceb4056e316fa7
peaceissa <issarae78@gmail.com>
2023-06-01 21:35:46 +0300

    added multiple colors to the hover effect

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const container = document.querySelector('#container');
 const gridSize = 17.4;
+const colors = ['purple','green','orange','pink','purple','blue','red','yellow','violet','gold'];
     for (let i = 0; i < gridSize * gridSize; i++) {
         const gridSquare = document.createElement('div');
         gridSquare.classList.add('grid-square');
         container.appendChild(gridSquare);
+
+        gridSquare.addEventListener('mouseover', function(){
+            const randomColor = colors[Math.floor(Math.random() * colors.length)]
+            gridSquare.style.backgroundColor = randomColor;
+        });
+        gridSquare.addEventListener('mouseout',function(){
+            gridSquare.style.backgroundColor = "";
+        });
     }
+
